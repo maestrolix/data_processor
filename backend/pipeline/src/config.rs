9dc55ce55cab153fc0297aa079837b437bc3290e
stepan <stepan>
2024-12-10 17:45:36 +0400

    Rewrote the use of environment variables to .env

--- backend/pipeline/src/config.rs
+++ backend/pipeline/src/config.rs
@@ -1,47 +1,65 @@
-use serde::Deserialize;
+use dotenv_config::EnvConfig;
 
-#[derive(Debug, Deserialize, Clone)]
-pub struct ModelData {
+#[derive(Debug, EnvConfig)]
+pub struct TextualModel {
+    #[env_config(name = "textual_model_path")]
     pub model_path: String,
+    #[env_config(name = "textual_model_name")]
     pub model_name: String,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, EnvConfig)]
+pub struct VisualModel {
+    #[env_config(name = "visual_model_path")]
+    pub model_path: String,
+    #[env_config(name = "visual_model_name")]
+    pub model_name: String,
+}
+
+#[derive(Debug, EnvConfig)]
+pub struct FacialDetectorModel {
+    #[env_config(name = "facial_detector_model_path")]
+    pub model_path: String,
+    #[env_config(name = "facial_detector_model_name")]
+    pub model_name: String,
+}
+
+#[derive(Debug, EnvConfig)]
+pub struct FacialRecognizerModel {
+    #[env_config(name = "facial_recognizer_model_path")]
+    pub model_path: String,
+    #[env_config(name = "facial_recognizer_model_name")]
+    pub model_name: String,
+}
+
+#[derive(Debug, EnvConfig)]
 pub struct Search {
-    pub visual: ModelData,
-    pub textual: ModelData,
+    pub visual: VisualModel,
+    pub textual: TextualModel,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, EnvConfig)]
 pub struct FacialProcessing {
-    pub detector: ModelData,
-    pub recognizer: ModelData,
+    pub detector: FacialDetectorModel,
+    pub recognizer: FacialRecognizerModel,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, EnvConfig)]
 pub struct Model {
     pub facial_processing: FacialProcessing,
     pub search: Search,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, EnvConfig)]
 pub struct Kafka {
+    #[env_config(name = "kafka_host")]
     pub host: String,
+    #[env_config(name = "kafka_port")]
     pub port: u16,
 }
 
-#[derive(Debug, Deserialize, Clone)]
+#[derive(Debug, EnvConfig)]
 pub struct Config {
     pub kafka: Kafka,
     pub model: Model,
-}
-
-impl Config {
-    pub fn new(config_file_name: &str) -> Self {
-        let config_file = std::env::var("CONFIG_FILE").unwrap_or(config_file_name.into());
-        let config_file = std::path::Path::new(&config_file);
-        let config = std::fs::read_to_string(config_file).unwrap();
-        let config: Config = toml::from_str(&config).unwrap();
-        config
-    }
 }
